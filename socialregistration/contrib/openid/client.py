8cb071f730928408cf47a0bfdfb646a23239839d
Ross Crawford <sendrossemail@gmail.com>
2012-03-17 16:56:42 -0700

    Added hooks for openid attributes

--- socialregistration/contrib/openid/client.py
+++ socialregistration/contrib/openid/client.py
@@ -1,12 +1,18 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from openid.consumer import consumer
+from openid.extensions import ax
 from socialregistration.clients import Client
 from socialregistration.contrib.openid.storage import OpenIDStore
 from socialregistration.settings import SESSION_KEY
 import urlparse
 
+AX_URLS = getattr(settings, 'SOCIALREGISTRATION_AX_URLS',
+    None)
+
 class OpenIDClient(Client):
+
     def __init__(self, session_data, endpoint_url):
         self.endpoint_url = endpoint_url
         self.store = OpenIDStore()
@@ -24,6 +30,14 @@ class OpenIDClient(Client):
     def get_redirect_url(self):
         auth_request = self.consumer.begin(self.endpoint_url)
         
+        if AX_URLS:
+            ax_request = ax.FetchRequest()
+            for value in AX_URLS:
+                url, is_required = value
+                ax_request.add(ax.AttrInfo(url, required=is_required))
+            # update with new items
+            auth_request.addExtension(ax_request)
+
         redirect_url = auth_request.redirectURL(self.get_realm(),
             self.get_callback_url())
         
